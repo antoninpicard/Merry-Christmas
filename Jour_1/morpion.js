8d6120319c9d47416b5dde2940c90b1b5fe77028
Antonin <antoninpicard0@gmail.com>
2022-12-01 20:09:00 +0100

    1.2

--- Jour_1/morpion.js
+++ Jour_1/morpion.js
@@ -19,7 +19,53 @@ function boxClicked(e){
     if(!spaces[id]){
         spaces[id] = currentPlayer;
         e.target.innerText = currentPlayer;
+
+        if(playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} has won!`
+            let winning_blocks = playerHasWon()
+
+            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
+            return
+        }
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT: X_TEXT
     }
+}
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+function playerHasWon(){
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])){
+            return [a,b,c]
+        }
+    }
+    return false;
+}
+
+
+
+restartBtn.addEventListener('click', restart)
+
+function restart(){
+    spaces.fill(null)
+
+    boxes.forEach(box => {
+        box.innerText = ''
+    })
+
+    currentPlayer = X_TEXT;
 }
 
 startGame()
