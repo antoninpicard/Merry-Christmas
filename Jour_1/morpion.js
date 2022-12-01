f00949a3d3756265bf363f3d404d01eefcc94ddf
Antonin <antoninpicard0@gmail.com>
2022-12-01 19:52:35 +0100

    1.1

--- Jour_1/morpion.js
+++ Jour_1/morpion.js
@@ -1,5 +1,25 @@
 let playerText = document.getElementById('playerText')
 let restartBtn = document.getElementById('restartBtn')
-let boxes = document.getElementById('box')
+let boxes = Array.from(document.getElementsByClassName('box'))
 
-console.log(boxes)
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
+
+const O_TEXT = "O"
+const X_TEXT = "X"
+let currentPlayer = X_TEXT
+let spaces = Array(9).fill(null)
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked));
+}
+
+function boxClicked(e){
+    const id = e.target.id;
+
+    if(!spaces[id]){
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+    }
+}
+
+startGame()
