1937340c364279a989c2880d513a84d138105694
Antonin <antoninpicard0@gmail.com>
2022-12-06 15:58:04 +0100

    gogame.go

---
+++ Jour_5/gogame.go
@@ -0,0 +1,100 @@
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Field struct {
+	s    [][]bool
+	w, h int
+}
+
+
+func NewField(w, h int) *Field {
+	s := make([][]bool, h)
+	for i := range s {
+		s[i] = make([]bool, w)
+	}
+	return &Field{s: s, w: w, h: h}
+}
+
+
+func (f *Field) Set(x, y int, b bool) {
+	f.s[y][x] = b
+}
+
+func (f *Field) Alive(x, y int) bool {
+	x += f.w
+	x %= f.w
+	y += f.h
+	y %= f.h
+	return f.s[y][x]
+}
+
+
+func (f *Field) Next(x, y int) bool {
+
+	alive := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if (j != 0 || i != 0) && f.Alive(x+i, y+j) {
+				alive++
+			}
+		}
+	}
+
+	return alive == 3 || alive == 2 && f.Alive(x, y)
+}
+
+type Life struct {
+	a, b *Field
+	w, h int
+}
+
+func NewLife(w, h int) *Life {
+	a := NewField(w, h)
+	for i := 0; i < (w * h / 4); i++ {
+		a.Set(rand.Intn(w), rand.Intn(h), true)
+	}
+	return &Life{
+		a: a, b: NewField(w, h),
+		w: w, h: h,
+	}
+}
+
+
+func (l *Life) Step() {
+
+	for y := 0; y < l.h; y++ {
+		for x := 0; x < l.w; x++ {
+			l.b.Set(x, y, l.a.Next(x, y))
+		}
+	}
+
+	l.a, l.b = l.b, l.a
+}
+
+func (l *Life) String() string {
+	var buf bytes.Buffer
+	for y := 0; y < l.h; y++ {
+		for x := 0; x < l.w; x++ {
+			b := byte(' ')
+			if l.a.Alive(x, y) {
+				b = '*'
+			}
+			buf.WriteByte(b)
+		}
+		buf.WriteByte('\n')
+	}
+	return buf.String()
+}
+
+func main() {
+	l := NewLife(40, 15)
+	for i := 0; i < 300; i++ {
+		l.Step()
+		fmt.Print("\x0c", l) 
+		time.Sleep(time.Second / 30)
+	}
+}
